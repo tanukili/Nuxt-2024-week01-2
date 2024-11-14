8b67b25c80843d25a5e030fd639fe1d77e11bb43
tanukile <backpacker320@gmail.com>
2024-11-15 00:38:00 +0800

    feat: 遠端 news 渲染

--- pages/index.vue
+++ pages/index.vue
@@ -8,14 +8,26 @@ const isLoading = ref(false);
 // API 路徑 : https://nuxr3.zeabur.app/api/v1/home/news/
 // 使用 ES6 fetch() 或是 axios.get() 串接 API
 // 切換 isLoading 狀態
-
-
+import axios from "axios";
+async function getNews(params) {
+  isLoading.value = !isLoading.value;
+  const res = await axios.get("https://nuxr3.zeabur.app/api/v1/home/news/");
+  if (res.data.status) {
+    newsList.value = [...res.data.result];
+  }
+  isLoading.value = !isLoading.value;
+}
+getNews();
 </script>
 
 <template>
   <div class="container">
     <h1>最新消息</h1>
-    <!-- <NewsCard v-for="..." :key="..."  v-bind="..." /> -->
+    <NewsCard
+      v-for="news in newsList"
+      :key="news.id"
+      v-bind="({ _id, title, image, description, createdAt, updatedAt } = news)"
+    />
 
     <ClientOnly>
       <Loading v-model:active="isLoading" />
