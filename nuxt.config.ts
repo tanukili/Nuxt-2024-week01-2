bdf110cb287d039b645e3ca755f3495825ef306b
tanukile <backpacker320@gmail.com>
2024-11-16 00:31:58 +0800

    chore: 套用 BS5 連結 active 樣式

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,11 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   css: ["@/assets/stylesheets/all.scss"],
+  router: {
+    options: {
+      // linkActiveClass 與 linkExactActiveClass 改用自定義樣式
+      linkActiveClass: "active",
+      linkExactActiveClass: "active",
+    },
+  },
 });
