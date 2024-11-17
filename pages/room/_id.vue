fe5f6a0a0e3c364b05011566d52f7cb37fc3447b
tanukile <backpacker320@gmail.com>
2024-11-17 23:04:27 +0800

    refactor: room 修改成嵌套式路由

---
+++ pages/room/_id.vue
@@ -0,0 +1,10 @@
+<script setup>
+const router = useRouter();
+</script>
+
+<template>
+  <h2>房型詳細頁面</h2>
+  <button @click="router.go(-1)" type="button">回上一頁</button>
+</template>
+
+<style scoped></style>
