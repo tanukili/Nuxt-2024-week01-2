5464bab059b8cb44d368847a93a75d8fce1428c6
tanukile <backpacker320@gmail.com>
2024-11-24 06:05:02 +0800

    feat: 註冊功能

--- pages/register.vue
+++ pages/register.vue
@@ -13,6 +13,8 @@ const userRegisteObject = ref({
     detail: "",
   },
 });
+const resetUserObject = JSON.parse(JSON.stringify(userRegisteObject.value));
+const isRegistering = ref(false);
 // 使用 sweetAlert2 套件顯示訊息
 // $swal.fire({
 //   position: "center",
@@ -21,16 +23,46 @@ const userRegisteObject = ref({
 //   showConfirmButton: false,
 //   timer: 1500,
 // });
+const signup = async () => {
+  isRegistering.value = !isRegistering.value;
+  try {
+    const res = await $fetch("/v1/user/signup", {
+      method: "post",
+      body: userRegisteObject.value,
+      baseURL: "https://nuxr3.zeabur.app/api",
+    });
+    $swal.fire({
+      position: "center",
+      icon: "success",
+      title: "註冊成功",
+      showConfirmButton: false,
+      timer: 3000,
+    });
+    Object.assign(resetUserObject, userRegisteObject.value); // reset
+  } catch (err) {
+    const { message } = err.response._data;
+    $swal.fire({
+      position: "center",
+      icon: "error",
+      title: message || "註冊失敗",
+      showConfirmButton: false,
+      timer: 3000,
+    });
+  }
+  isRegistering.value = !isRegistering.value;
+};
 </script>
 
 <template>
   <div class="bg-light py-3 py-md-5 vh-100">
     <div class="container">
+      {{ userRegisteObject }}
+      {{ resetUserObject }}
       <div class="row justify-content-md-center">
         <div class="col-12 col-md-11 col-lg-8 col-xl-7 col-xxl-6">
           <div class="bg-white p-4 p-md-5 rounded shadow-sm">
             <h2 class="h3 mb-4">會員註冊</h2>
-            <form @submit.prevent="">
+            <form @submit.prevent="signup">
               <div class="form-floating mb-4">
                 <input
                   type="text"
@@ -38,6 +70,7 @@ const userRegisteObject = ref({
                   id="firstName"
                   placeholder="王小明"
                   required
+                  v-model="userRegisteObject.name"
                 />
                 <label for="firstName"
                   >姓名 <span class="text-danger">*</span></label
@@ -52,6 +85,7 @@ const userRegisteObject = ref({
                   placeholder="example@gmail.com"
                   pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
                   required
+                  v-model="userRegisteObject.email"
                 />
                 <label for="email"
                   >信箱 <span class="text-danger">*</span></label
@@ -66,6 +100,7 @@ const userRegisteObject = ref({
                   placeholder="請輸入 8 碼以上密碼"
                   pattern=".{8,}"
                   required
+                  v-model="userRegisteObject.password"
                 />
                 <label for="password"
                   >密碼 <span class="text-danger">*</span></label
@@ -80,6 +115,7 @@ const userRegisteObject = ref({
                   placeholder="0912345678"
                   pattern="(\+886|0)?9\d{8}|(\+886|0)?2\d{8}|\d{3}-\d{4}-\d{4}"
                   required
+                  v-model="userRegisteObject.phone"
                 />
                 <label for="phone">電話</label>
               </div>
@@ -90,6 +126,7 @@ const userRegisteObject = ref({
                   class="form-control"
                   id="dateInput"
                   required
+                  v-model="userRegisteObject.birthday"
                 />
                 <label for="dateInput">出生年月日</label>
               </div>
@@ -104,6 +141,7 @@ const userRegisteObject = ref({
                       placeholder="100"
                       pattern="\d{3,}"
                       required
+                      v-model.number="userRegisteObject.address.zipcode"
                     />
                     <label for="zipcode">郵遞區號</label>
                   </div>
@@ -116,13 +154,18 @@ const userRegisteObject = ref({
                       id="address"
                       placeholder="台北市中正區重慶南路一段"
                       required
+                      v-model="userRegisteObject.address.detail"
                     />
                     <label for="address">詳細地址</label>
                   </div>
                 </div>
               </div>
 
-              <button class="btn btn-lg btn-primary w-100" type="submit">
+              <button
+                class="btn btn-lg btn-primary w-100"
+                type="submit"
+                :disabled="isRegistering"
+              >
                 註冊
               </button>
             </form>
