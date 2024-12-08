99123a1833fcd23ac25fd59787e4164108fc2707
tanukile <backpacker320@gmail.com>
2024-12-09 03:51:54 +0800

    feat: directive 大小寫切換、時間格式

--- plugins/textformat.js
+++ plugins/textformat.js
@@ -1,1 +1,11 @@
-export default defineNuxtPlugin((nuxtApp) => {});
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.directive("textformat", {
+    mounted(el, binding) {
+      const newTtext =
+        binding.arg === "uppercase"
+          ? el.textContent.toUpperCase()
+          : el.textContent.toLowerCase();
+      el.textContent = newTtext;
+    },
+  });
+});
--- plugins/timeformat.js
+++ plugins/timeformat.js
@@ -1,1 +1,13 @@
-export default defineNuxtPlugin((nuxtApp) => {});
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.directive("timeformat", {
+    mounted(el) {
+      const date = new Date(parseInt(el.textContent, 10));
+      const setformat = (number) => `${number}`.padStart(2, "0");
+      const month = setformat(date.getMonth() + 1);
+      const time = date.toTimeString().slice(0, 8);
+      el.textContent = `${date.getFullYear()}-${month}-${setformat(
+        date.getDate()
+      )} ${time}`;
+    },
+  });
+});
